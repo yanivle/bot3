63457becaedaffce4ee06a7af6106f55e00bdb28
Yaniv Leviathan <leviathan@leviathan-macbookpro3.roam.corp.google.com>
2018-09-11 10:20:59 -0400

    VarSpec - WIP (not used)

--- var_spec.py
+++ var_spec.py
@@ -1,30 +1,45 @@
-import functools
-import prioritized_keys
+from dataclasses import dataclass, field
+from typing import List
 
 
-def getKeysFromState(state):
-    return set(state.statements.statements.keys()) | set(state.predictions.statements.keys())
-
-
-def getKeysFromStateContainer(container):
-    return getKeysFromState(container.state)
-
-
-def getKeysFromStateContainerList(container_list):
-    return functools.reduce(lambda x, y: x | y, (getKeysFromStateContainer(container) for container in container_list), set())
-
-
-class VarSpec(prioritized_keys.PrioritizedKeys):
-    def extendFromStateContainers(self, container_list, default_priority=0.0):
-        keys_set = getKeysFromStateContainerList(container_list)
-        for key in keys_set:
-            if key not in self.keys:
-                self.keys[key] = default_priority
+@dataclass
+class Variable(object):
+    name: str
+    correctible: bool = False
 
     @staticmethod
-    def fromFileAndUpdate(filename, container_list):
-        var_spec = VarSpec({})
-        var_spec.load(filename)
-        var_spec.extendFromStateContainers(container_list)
-        var_spec.save(filename)
-        return var_spec
+    def fromText(text):
+        name
+
+
+@dataclass
+class VarSpec(object):
+    vars: List[Variable]
+
+
+# def getKeysFromState(state):
+#     return set(state.statements.statements.keys()) | set(state.predictions.statements.keys())
+#
+#
+# def getKeysFromStateContainer(container):
+#     return getKeysFromState(container.state)
+#
+#
+# def getKeysFromStateContainerList(container_list):
+#     return functools.reduce(lambda x, y: x | y, (getKeysFromStateContainer(container) for container in container_list), set())
+#
+#
+# class VarSpec(prioritized_keys.PrioritizedKeys):
+#     def extendFromStateContainers(self, container_list, default_priority=0.0):
+#         keys_set = getKeysFromStateContainerList(container_list)
+#         for key in keys_set:
+#             if key not in self.keys:
+#                 self.keys[key] = default_priority
+#
+#     @staticmethod
+#     def fromFileAndUpdate(filename, container_list):
+#         var_spec = VarSpec({})
+#         var_spec.load(filename)
+#         var_spec.extendFromStateContainers(container_list)
+#         var_spec.save(filename)
+#         return var_spec
